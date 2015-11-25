f5f017a1a96c30b3fb909121faa518f0003e077c
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-11-25 16:59:41 +0200

    Change

--- src/application/Demo.java
+++ src/application/Demo.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
@@ -10,10 +11,11 @@ import java.nio.file.Paths;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class Demo {
-    public static void main(String[] args) throws IOException {
-        File file = new File();
-        System.out.println("Enter text!");
-        Path path= Paths.get("file.txt");
-        file.readConsolewriteFile(new InputStreamReader(System.in), "@",path);
-    }
+  public static void main(String[] args) throws IOException {
+    File file = new File();
+    System.out.println("Enter text!");
+    Path path = Paths.get("file.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    file.readConsolewriteFile(reader, "@", path);
+  }
 }
--- src/application/File.java
+++ src/application/File.java
@@ -11,26 +11,27 @@ import java.util.Scanner;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class File {
-    public void readConsolewriteFile(Reader reader, String separator, Path path) throws IOException {
-        BufferedWriter out = null;
-        BufferedReader in = null;
-        Charset charset = Charset.forName("UTF-8");
-        try {
-            in = new BufferedReader(reader);
-            out = Files.newBufferedWriter(path, charset);
-            String line;
-            do {
-                line = in.readLine();
-                out.write(line);
-                out.write("\n");
-            } while (!line.equals(separator));
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-        }
+  private BufferedReader in;
+  private Writer out;
+
+  public void readConsolewriteFile(Reader reader, String separator, Path path) throws IOException {
+    Charset charset = Charset.forName("UTF-8");
+    try {
+      in = new BufferedReader(reader);
+      out = Files.newBufferedWriter(path, charset);
+      String line;
+      do {
+        line = in.readLine();
+        out.write(line);
+        out.write("\n");
+      } while (!line.equals(separator));
+    } finally {
+      if (in != null) {
+        in.close();
+      }
+      if (out != null) {
+        out.close();
+      }
     }
+  }
 }
