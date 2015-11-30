68cd365874c350b587022e0dca4928c870415155
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-11-30 17:05:10 +0200

    Change method read

--- src/reverse/File.java
+++ src/reverse/File.java
@@ -15,12 +15,10 @@ public class File {
   public File(String fileName) {
     this.fileName = fileName;
   }
-
   public void reverse() throws IOException {
     File buf = new File(fileName);
     StringBuffer buffer = buf.read();
     BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-    buffer.reverse();
     writer.write(buffer.toString());
     writer.close();
   }
@@ -28,18 +26,14 @@ public class File {
   private StringBuffer read() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(fileName));
     StringBuffer buffer = new StringBuffer();
-    try {
-      String text;
-
-      while ((text = reader.readLine()) != null) {
-        buffer.append("\n");
-        buffer.append(text);
-      }
-    } finally {
-      if (reader != null) {
-        reader.close();
-      }
+    String text;
+    while ((text = reader.readLine()) != null) {
+      buffer.append("\n");
+      buffer.append(text);
     }
+    reader.close();
+    buffer.reverse();
+
     return buffer;
   }
 }
