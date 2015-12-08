5c04c96a58707e81531386ed5082c3134e49ff3c
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-12-08 16:18:57 +0200

    Fixed

--- src/transfer/TransferObject.java
+++ src/transfer/TransferObject.java
@@ -18,12 +18,11 @@ public class TransferObject {
   }
 
   public void transfer(InputStream in, OutputStream out, int startByte, int limit) throws IOException {
-    int size = in.available();
-    byte[] data = new byte[size];
+    byte[] data = new byte[2048];
     int c = in.read(data);
     if (c != -1) {
-      if (startByte + limit > size) {
-        out.write(data, startByte, size - startByte);
+      if (startByte + limit > data.length) {
+        out.write(data, startByte, data.length - startByte);
       } else {
         out.write(data, startByte, limit);
       }
