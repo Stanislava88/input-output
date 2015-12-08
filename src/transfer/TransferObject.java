634503c63871f297f1c0da7c2900856a23a7ffa4
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-12-08 09:27:33 -0800

    Fixed

--- src/transfer/TransferObject.java
+++ src/transfer/TransferObject.java
@@ -8,26 +8,25 @@ import java.io.OutputStream;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class TransferObject {
-  public void transfer(InputStream in, OutputStream out) throws IOException {
-    int size = in.available();
-    byte[] data = new byte[size];
-    int c = in.read(data);
-    if (c != -1) {
-      out.write(data, 0, c);
+    public void transfer(InputStream in, OutputStream out) throws IOException {
+        byte[] data = new byte[2048];
+        int c = in.read(data);
+        if (c != -1) {
+            out.write(data, 0, c);
+        }
     }
-  }
 
-  public void transfer(InputStream in, OutputStream out, int startByte, int limit) throws IOException {
-    byte[] data = new byte[2048];
-    int c = in.read(data);
-    if (c != -1) {
-      if (startByte + limit > data.length) {
-        out.write(data, startByte, data.length - startByte);
-      } else {
-        out.write(data, startByte, limit);
-      }
+    public void transfer(InputStream in, OutputStream out, int startByte, int limit) throws IOException {
+        byte[] data = new byte[2048];
+        int c = in.read(data);
+        if (c != -1) {
+            if (startByte + limit > data.length) {
+                out.write(data, startByte, data.length - startByte);
+            } else {
+                out.write(data, startByte, limit);
+            }
+        }
     }
-  }
 }
 
 
