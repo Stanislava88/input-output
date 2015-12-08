57f2867157d36bf3ab59af33ddd4a2e5125505e2
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-12-08 11:18:11 -0800

    Changed

--- src/transfer/Demo.java
+++ src/transfer/Demo.java
@@ -11,23 +11,23 @@ import java.nio.file.Paths;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class Demo {
-  public static void main(String[] args) throws IOException {
-    Path source = Paths.get("source.txt");
-    Path destination = Paths.get("destination.txt");
-    FileInputStream input = new FileInputStream(String.valueOf(source));
-    FileOutputStream output = new FileOutputStream(String.valueOf(destination));
-    try {
-      int startByte = 5;
-      TransferObject obj = new TransferObject();
-//      obj.transfer(input, output, startByte);
-      obj.transfer(input, output, startByte, 200);
-    } finally {
-      if (input != null) {
-        input.close();
-      }
-      if (output != null) {
-        output.close();
-      }
+    public static void main(String[] args) throws IOException {
+        Path source = Paths.get("source.txt");
+        Path destination = Paths.get("destination.txt");
+        FileInputStream input = new FileInputStream(String.valueOf(source));
+        FileOutputStream output = new FileOutputStream(String.valueOf(destination));
+        try {
+            int startByte = 0;
+            TransferObject obj = new TransferObject();
+//            obj.transfer(input, output);
+            obj.transfer(input, output, startByte, 200);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+        }
     }
-  }
 }
--- src/transfer/TransferObject.java
+++ src/transfer/TransferObject.java
@@ -1,29 +1,34 @@
 package transfer;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 /**
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class TransferObject {
     public void transfer(InputStream in, OutputStream out) throws IOException {
-        byte[] data = new byte[2048];
-        int c = in.read(data);
-        if (c != -1) {
-            out.write(data, 0, c);
-        }
+        byte[] data = new byte[1024];
+        int c;
+        while ((c = in.read(data)) > 0)
+            if (c != -1) {
+                out.write(data, 0, c);
+            }
     }
 
     public void transfer(InputStream in, OutputStream out, int startByte, int limit) throws IOException {
-        byte[] data = new byte[2048];
-        int c = in.read(data);
-        if (c != -1) {
-            if (startByte + limit > data.length) {
-                out.write(data, startByte, data.length - startByte);
-            } else {
-                out.write(data, startByte, limit);
+        byte[] data = new byte[1024];
+        int c;
+        while ((c = in.read(data)) > 0) {
+            if (c != -1) {
+                if (startByte + limit > data.length) {
+                    out.write(data, startByte, data.length - startByte);
+                } else {
+                    out.write(data, startByte, limit);
+                }
             }
         }
     }
