0de1b464eb812e0a0d1ca0175afc4642b6b24f8f
Stanislava Kaukova <sisiivanovva@gmail.com>
2015-12-16 10:51:54 -0800

    Stream closed

--- src/serialization/DataClass.java
+++ src/serialization/DataClass.java
@@ -12,26 +12,27 @@ import java.lang.Object;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class DataClass {
-  public void saveObject(OutputStream out, Object o) throws IOException {
-    try {
-      ObjectOutputStream output = new ObjectOutputStream(out);
-      output.writeObject(o);
-      output.flush();
-    } catch (IOException ex) {
-      ex.printStackTrace();
+    public void saveObject(OutputStream out, Object o) throws IOException {
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(out);
+            output.writeObject(o);
+            output.flush();
+            out.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-  }
 
-public Object getObject(InputStream in) throws IOException, ClassNotFoundException {
-    Flower flower = null;
-    try {
-      ObjectInputStream input = new ObjectInputStream(in);
-      flower = (Flower) input.readObject();
-      input.close();
+    public Object getObject(InputStream in) throws IOException, ClassNotFoundException {
+        Flower flower = null;
+        try {
+            ObjectInputStream input = new ObjectInputStream(in);
+            flower = (Flower) input.readObject();
+            input.close();
 
-    } catch (IOException ex) {
-      ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return flower;
     }
-    return flower;
-  }
 }
